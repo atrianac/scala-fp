d7a3be666acffd87f4dbcebf6c54251f1ae963f9
atrianac <alejandro.triana@outlook.com>
2015-06-23 06:00:54 -0500

    Final implementation of Either type

--- exception-handling/src/com/scala/exceptionhandling/Either.scala
+++ exception-handling/src/com/scala/exceptionhandling/Either.scala
@@ -7,7 +7,29 @@ trait Either[+E, +A] {
     case Left(e) => Left(e)
   }
   
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[E, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => f(a)
+    case Left(e) => Left(e)
+  }
+  
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => this
+    case _ => b
+  }
+  
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+    this flatMap ( _a => b flatMap (_b => Right(f(_a, _b))) )
+  }
+  
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es match {
+    case Nil => Right(Nil)
+    case x :: xs => x flatMap (v => sequence (xs) map ( vi => v :: vi ))
+  }
+  
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
+    case Nil => Right(Nil)
+    case x :: xs => f(x) flatMap ( v => traverse(xs)(f) map (vi => v :: vi) )
+  }
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
