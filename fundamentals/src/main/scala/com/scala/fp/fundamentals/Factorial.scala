540841a6b199159af7bc9fa5db5e2953dc6860ce
Alejandro <alejandro.triana@outlook.com>
2016-06-11 09:07:31 -0500

    Update Factorial.scala

--- fundamentals/src/main/scala/com/scala/fp/fundamentals/Factorial.scala
+++ fundamentals/src/main/scala/com/scala/fp/fundamentals/Factorial.scala
@@ -1,10 +1,9 @@
 package com.scala.fp.fundamentals
 
 object Factorial  {
-	
 	def factorial(n: Int) : Int = {
 		@annotation.tailrec
-    def go(n: Int, acc: Int): Int = {
+    		def go(n: Int, acc: Int): Int = {
 			if (n <= 0) acc
 			else go(n - 1, n * acc)
 		}
