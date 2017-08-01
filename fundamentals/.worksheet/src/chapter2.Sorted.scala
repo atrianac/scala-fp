ea1ed49200e0e3d1bbd564256e6e5c14e0564dcb
alejandro.triana@endava.com <alejandro.triana@endava.com>
2017-08-01 09:41:17 -0500

    Delete IDE files

--- fundamentals/.worksheet/src/chapter2.Sorted.scala
+++
@@ -1,5 +0,0 @@
-package chapter2
-
-object Sorted {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(77); 
-  println("Welcome to the Scala worksheet")}
-}
